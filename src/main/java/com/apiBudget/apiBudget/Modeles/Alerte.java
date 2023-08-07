1c7d4f24d3323fb598cc995e9fbeb2af3179a424
unknown <tour.idrissa@odc.local>
2023-08-07 15:43:13 +0000

    conflit commit

--- src/main/java/com/apiBudget/apiBudget/Modeles/Alerte.java
+++ src/main/java/com/apiBudget/apiBudget/Modeles/Alerte.java
@@ -2,13 +2,5 @@ package com.apiBudget.apiBudget.Modeles;
 
 public class Alerte {
 
-  //  @Id
-    //@GeneratedValue(strategy = GenerationType.IDENTITY)
-    //private Long id;
-   // private String message;
-   // private int date ;
-    //private long vue;
 
-  //  @OneToMany(mappedBy = "utilisateurId",cascade = CascadeType.ALL)
-   // private List<Budget> budgetList;
 }
